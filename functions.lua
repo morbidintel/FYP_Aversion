890cb6f407e4f0feecfa8c54aa935a01b2293a2a
morbidintel <hanged94@gmail.com>
2014-04-23 11:14:05 +0800

    Added isInRange function

--- functions.lua
+++ functions.lua
@@ -4,24 +4,24 @@ function printTable(s, l, i) -- recursive Print (structure, limit, indent)
 	i = i or ""	-- default item limit, indent string
 
 	if (l<1) then
-		print "ERROR: Item limit reached."
+		print "Item limit reached."
 		return l-1
 	end
 
 	local ts = type(s)
 
-	if (ts ~= "table") then
+	if ts ~= "table" then
 		print(i, ts, s)
 		return l - 1
 	end
 
-	print (i .. ts)           -- print "table"
+	print (i .. ts)		   -- print "table"
 
 	for k,v in pairs(s) do  -- print "[KEY] VALUE"
 
 		if tostring(k) == "__index" then
 
-			print(i .. ts, s)
+			print(i.."["..tostring(k).."]\t", ts, s)
 
 		else
 
@@ -37,20 +37,100 @@ function printTable(s, l, i) -- recursive Print (structure, limit, indent)
 
 end
 
-function mergeTable(t1, t2)
+function clone(t, target) -- deep-copy a table
 
-	for k,v in pairs(t2) do
+	if type(t) ~= "table" then return t end
 
-		if type(v) == "table" and type(t1[k] or false) == "table" then
+	local meta = getmetatable(t)
 
-			mergeTable(t1[k], t2[k])
+	target = target or {}
 
+	for k, v in pairs(t) do
+
+		if type(v) == "table" and k ~= "__index" then
+			target[k] = clone(v)
 		else
-
-			t1[k] = v
-
+			target[k] = v
 		end
 
+	end
+
+	setmetatable(target, meta)
+
+	return target
+
+end
+
+function copy(t, target) -- shallow-copy a table
+
+	if type(t) ~= "table" then return t end
+
+	local meta = getmetatable(t)
+	target = target or {}
+
+	for k, v in pairs(t) do target[k] = v end
+
+	setmetatable(target, meta)
+
+	return target
+
+end
+
+-- merges two tables and returns the result
+function mergeTable(t1, t2)
+
+	local tempTable = {}
+
+	tempTable = clone(t1, tempTable)
+	tempTable = clone(t2, tempTable)
+
+	return tempTable
+
+end
+
+-- create a table with unmodifiable values
+-- values in a table inside of a const_table are modifiable
+function const_table(table)
+
+	return setmetatable(
+		{},
+		{
+			__index = table,
+			__newindex = 
+				function(table, key, value)
+					error("Attempt to modify read-only table")
+				end,
+			__metatable = false
+		}
+	);
+
+end
+
+-- check if value is in a range
+function isInRange(value, min, max)
+
+	-- check the type of value
+	local vType = type(value)
+
+	if vType == "table" then
+		-- if we have a table
+
+		-- go through all the values in the table
+		for k,v in pairs(value) do
+			-- if one value is not in range, no need to check the rest
+			if not isInRange(v,min,max) return false
+		end
+
+		-- all values are in range, return true
+		return true
+
+	elseif vType == "number" and type(min) == "number" and type(max) == "number" then
+		-- make sure all the stuff we are checking are numbers
+
+		return value > min and value < max
+
+	else
+		return false -- "NaN"
 	end
 
 end
